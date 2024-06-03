24154a8c44dad957e48bb0a688f47944c9520a22
Randula2006 <randula30085@gmail.com>
2024-06-03 22:28:53 +0530

    file number checking function created

--- rename_checker.py
+++ rename_checker.py
@@ -47,55 +47,92 @@ import shutil
 #  the data can either have a file name or a whole directory name
 # by using move2 we can save all the meta data in the files 
 
-import os
 
-from numpy import append
 
 file_number_array = []
+
+
 
 def phrase_upper_check(phrase_alphebet_lower):
     return [s.upper() for s in phrase_alphebet_lower]
 
 
-def rename_checker(phrase, phrase_uppercase , file_name):
-    # for phrase, phrase_uppercase in zip(alphebet, alphabet_uppercase):
-        
-        if (file_name[0] in phrase) or (file_name[0] in phrase_uppercase):
-             # renaming checker if the value starts with a letter 
-             file_name_without_ext, file_ext = os.path.splitext(file_name)
-        
-             # Strip and split the file name
-             clear_file, file_num = file_name_without_ext.strip().split('_')
-        
-             # Reorder the file name
-             file_names_reordered = f'{file_num}_{clear_file}{file_ext}'
-             # printing the renamed file 
-             print(f'Renaming {file_name} to {file_names_reordered}')
-        
-             # Rename the file
-             os.rename(file_name, file_names_reordered)
-             
-            #  appending the value file_number to file_number_array list 
-             global file_number_array
-             file_number_array.append(file_num)
-             
-             
-        else:
-                print('No need to change format') 
-     
 
-os.chdir(r'C:\Users\Randula\Desktop\Programming\python\Rebooting\projects\Image sorter\images')
+def rename_checker(phrase, phrase_uppercase, file_name):
+    
+    print(f'Checking file: {file_name}')  # Debugging statement
+    
+    # evaluating if the file has the alphebet lower or uppercase in the filename 
+    if (file_name[0] in phrase) or (file_name[0] in phrase_uppercase):
+        
+        try:
+            # Renaming checker if the value starts with a letter
+            file_name_without_ext, file_ext = os.path.splitext(file_name)
+            # Ensure the filename contains an underscore and can be split into two parts
+            
+            if '_' in file_name_without_ext:
+                # Strip and split the file name
+                parts = file_name_without_ext.strip().split('_')
+                
+                # checking the length of the file 
+                if len(parts) == 2 and parts[1].isdigit():
+                    
+                    clear_file, file_num = parts
+                    # Reorder the file name
+                    file_names_reordered = f'{file_num}_{clear_file}{file_ext}'
+                    # Printing the renamed file 
+                    print(f'Renaming {file_name} to {file_names_reordered}')
+                    # Rename the file
+                    os.rename(file_name, file_names_reordered)
+                    # Append the file number to the global array
+                    
+                    
+                    global file_number_array
+                    file_number_array.append(int(file_num))  # Ensure file_num is stored as an integer
+                    print(f'Appended {file_num} to file_number_array')  # Debugging statement
+                    
+                else:
+                    print(f'Skipping file {file_name} due to unexpected format: {parts}')
+            else:
+                print(f'Skipping file {file_name} due to missing underscore')
+                
+                
+        except Exception as e:
+            print(f'Error processing file {file_name}: {e}')  # Debugging statement
+    else:
+         global file_number_array
+         file_number_array.append(int(file_num))  # Ensure file_num is stored as an integer
+         print(f'Appended {file_num} to file_number_array')  # Debugging statement
+         
+         print('No need to change format') 
+        
 
+# Change to the directory containing the files
+os.chdir(r'C:\Users\Randula\Desktop\Programming\python\Rebooting\projects\Image Sorter - Github\Image-Sorter\images')
 print(os.getcwd())
 
 
-
-alphebet = ['a', 'b' ,'c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+alphebet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 alphabet_uppercase = phrase_upper_check(alphebet)
-# print(alphabet_uppercase)
+
+
 
 for file_name in os.listdir():
-    
-    rename_checker(alphebet, alphabet_uppercase , file_name)
-    
+    rename_checker(alphebet, alphabet_uppercase, file_name)
+
+print("File numbers collected:", file_number_array)
+
+
+# Find and print the lowest number in the file_number_array
+if file_number_array:
+    lowest_number = min(file_number_array)
+    print(f"The lowest file number is: {lowest_number}")
+else:
+    print("No file numbers collected.")
+
+
+##################################################################
+# posible errors
+
+# if the file is not chcked the value is not sent to the array of integers
