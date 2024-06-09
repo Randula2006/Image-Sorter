fbf32bed159361e0ce3a871ec787d576df7fc9f9
Randula2006 <randula30085@gmail.com>
2024-06-09 12:37:26 +0530

    added automatic renaming and getting the lowest image_num

---
+++ function-creater.py
@@ -0,0 +1,13 @@
+
+import os
+import customtkinter
+
+
+image_num = ''
+if (image_num <= 0) :
+    print('images cant have negative numbers ...so go to the next closest image')
+    
+    
+
+else:
+    print('loop continues')
--- sorter.py
+++ sorter.py
@@ -6,11 +6,137 @@ import shutil
 from pathlib import Path
 
 # changing the directory to the folder with the images
-os.chdir(r'C:\Users\Randula\Desktop\Programming\python\Rebooting\projects\Image sorter\images')
+os.chdir(r'C:\Users\Randula\Desktop\Programming\python\Rebooting\projects\Image Sorter - Github\Image-Sorter\images')
+# have to change the file path when changing the directory 
 
 
-image_num =  int(input("enter the first value of the image you have")) #this will be replaced with the image renamer files and automatically add the image first value
+
+file_number_array = []
+
+
+
+def phrase_upper_check(phrase_alphebet_lower):
+    return [s.upper() for s in phrase_alphebet_lower]
+
+
+
+def rename_checker(phrase, phrase_uppercase, file_name):
     
+    print(f'Checking file: {file_name}')  # Debugging statement
+    
+    # evaluating if the file has the alphebet lower or uppercase in the filename 
+    if (file_name[0] in phrase) or (file_name[0] in phrase_uppercase):
+        
+        try:
+            # Renaming checker if the value starts with a letter
+            file_name_without_ext, file_ext = os.path.splitext(file_name)
+            # Ensure the filename contains an underscore and can be split into two parts
+            
+            if '_' in file_name_without_ext:
+                # Strip and split the file name
+                parts = file_name_without_ext.strip().split('_')
+                
+                # checking the length of the file 
+                if len(parts) == 2 and parts[1].isdigit():
+                    
+                    clear_file, file_num = parts
+                    # Reorder the file name
+                    file_names_reordered = f'{file_num}_{clear_file}{file_ext}'
+                    # Printing the renamed file 
+                    print(f'Renaming {file_name} to {file_names_reordered}')
+                    # Rename the file
+                    os.rename(file_name, file_names_reordered)
+                    # Append the file number to the global array
+                    
+                    
+                    global file_number_array
+                    file_number_array.append(int(file_num))  # Ensure file_num is stored as an integer
+                    print(f'Appended {file_num} to file_number_array')  # Debugging statement
+                    
+                else:
+                    print(f'Skipping file {file_name} due to unexpected format: {parts}')
+            else:
+                print(f'Skipping file {file_name} due to missing underscore')
+                
+                
+        except Exception as e:
+            print(f'Error processing file {file_name}: {e}')  # Debugging statement
+    else:
+        # trying to fix if the files are already have the correct format 
+         file_name_without_ext, file_ext = os.path.splitext(file_name)
+         parts = file_name_without_ext.strip().split('_')
+        #  the below two lines of code can sometimes break the function as they will  not output the correct format 
+         file_num , clear_file = parts
+         file_names_reordered = f'{file_num}_{clear_file}{file_ext}'
+         os.rename(file_name, file_names_reordered)
+         
+         file_number_array
+         file_number_array.append(int(file_num))  # Ensure file_num is stored as an integer
+         print(f'Appended {file_num} to file_number_array')  # Debugging statement
+         
+         print('No need to change format') 
+        
+
+# Change to the directory containing the files
+os.chdir(r'C:\Users\Randula\Desktop\Programming\python\Rebooting\projects\Image Sorter - Github\Image-Sorter\images')
+print(os.getcwd())
+
+
+alphebet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+alphabet_uppercase = phrase_upper_check(alphebet)
+
+
+
+for file_name in os.listdir():
+    rename_checker(alphebet, alphabet_uppercase, file_name)
+
+print("File numbers collected:", file_number_array)
+
+
+# Find and print the lowest number in the file_number_array
+if file_number_array:
+    lowest_number = min(file_number_array)
+    image_num = lowest_number
+    print(f"The lowest file number is: {lowest_number}")
+else:
+    print("No file numbers collected.")
+
+
+##################################################################
+# posible errors
+
+# if the file is not chcked the value is not sent to the array of integers
+
+
+#upper for is for that #this will be replaced with the image renamer files and automatically add the image first value
+
+def image_not_exists():
+    global image_num
+    
+    for num in file_number_array:
+        if num > image_num:
+            image_num = num
+            break
+        else:
+            image_num = image_num
+            print('problem exists-have to fix it')
+    update_image()
+
+def image_decending():
+    global image_num
+    
+    for num in reversed(file_number_array):
+        if num < image_num:
+            image_num = num
+            break
+        else:
+            image_num = image_num
+            print('file is not decending')
+    
+    update_image()
+    
+
 def denied(event = None):
     global image_num
     # creating a folder named to_be_deleted or if exists send the image to that folder
@@ -20,6 +146,10 @@ def denied(event = None):
     shutil.move('{}_IMG.JPG'.format(image_num) , deletion_folder)
     
     image_num += 1  
+    
+    if image_num not in file_number_array:
+        image_not_exists()
+        
     print('image has sent to To_Be_Deleted folder')
     update_image()
         
@@ -45,6 +175,7 @@ def pre_image(event = None):
     
     image_num = image_num - 1
     print('pevious image')
+    image_decending()
     update_image()   
     
     return image_num
@@ -69,7 +200,14 @@ def update_image():
         image_label.configure(image = image)
     else:
         print(f'Image {image_path} does not exists.')
-    
+        image_not_exists()
+        
+        
+        #add the file_number_array to go to the closest next file
+        
+        
+        
+ 
 # GUi area 
 
 customtkinter.set_appearance_mode("system")
